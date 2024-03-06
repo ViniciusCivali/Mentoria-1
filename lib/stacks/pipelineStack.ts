2ccf48db6643d32a79911088b617eb886b6e35c6
vinicihd <vinicihd@amazon.com>
2024-03-06 14:44:51 -0300

    Creating test queue

--- lib/stacks/pipelineStack.ts
+++ lib/stacks/pipelineStack.ts
@@ -1,7 +1,4 @@
 import * as cdk from 'aws-cdk-lib';
-import * as codepipeline from '@aws-cdk/aws-codepipeline';
-import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
-import * as codebuild from '@aws-cdk/aws-codebuild';
 import { Construct } from 'constructs';
 import {CodePipeline, CodePipelineSource, ShellStep} from "aws-cdk-lib/pipelines";
 import {PipelineAppAlphaStage} from "../Stages/pipelineAppAlphaStage";
--- lib/stacks/s3Stack.ts
+++ lib/stacks/s3Stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import {Bucket} from "aws-cdk-lib/aws-s3";
+import {Queue} from "aws-cdk-lib/aws-sqs";
+import {Duration} from "aws-cdk-lib";
 
 export class S3Stack extends cdk.Stack {
     constructor(scope: Construct, id: string, props: cdk.StackProps) {
@@ -8,6 +10,10 @@ export class S3Stack extends cdk.Stack {
 
         const s3BucketTest = new Bucket(this, "s3BucketTest", {
             bucketName: "s3-bucket-test-1432"
+        })
+
+        const queue = new Queue(this, "queue", {
+            visibilityTimeout: Duration.seconds(300)
         })
     }
 }
